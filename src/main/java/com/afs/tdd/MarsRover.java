e1d58e8c9d90b192c16031e4d816b12a567ef727
rayna806 <LUORA2@OOCL.COM>
2025-09-07 13:55:09 +0800

    Test:create Direction enum & Command enum,and turnLeft&turnRight method in Direction enum class

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,85 +1,39 @@
 package com.afs.tdd;
 
 public class MarsRover {
-    public static final String Move = "M";
-    public static final String Left = "L";
-    public static final String Right = "R";
-    public static final String North = "N";
-    public static final String South = "S";
-    public static final String East = "E";
-    private int x;
-    private int y;
-    private String direction;
+    private Position position;
+    private Direction direction;
 
     public MarsRover(int x, int y, String direction) {
-        this.x = x;
-        this.y = y;
-        this.direction = direction;
-
+        this.position = new Position(x, y);
+        this.direction = Direction.fromCode(direction);
     }
 
     public int getX() {
-        return x;
+        return position.getX();
     }
 
     public int getY() {
-        return y;
+        return position.getY();
     }
 
     public void executeCommand(String command) {
-        if (Move.equals(command)) {
-            switch (direction) {
-                case North:
-                    y++;
-                    break;
-                case South:
-                    y--;
-                    break;
-                case East:
-                    x++;
-                    break;
-                default:
-                    x--;
-                    break;
-            }
+        Command cmd = Command.fromCode(command);
+        switch (cmd) {
+            case MOVE:
+                position = direction.move(position);
+                break;
+            case LEFT:
+                direction = direction.turnLeft();
+                break;
+            case RIGHT:
+                direction = direction.turnRight();
+                break;
         }
-        if (Left.equals(command)) {
-            switch (direction) {
-                case North:
-                    direction = "W";
-                    break;
-                case "W":
-                    direction = South;
-                    break;
-                case South:
-                    direction = East;
-                    break;
-                default:
-                    direction = North;
-                    break;
-            }
-        }
-        else if (Right.equals(command)) {
-            switch (direction) {
-                case North:
-                    direction = East;
-                    break;
-                case East:
-                    direction = South;
-                    break;
-                case South:
-                    direction = "W";
-                    break;
-                default:
-                    direction = North;
-                    break;
-            }
-        }
-
     }
 
     @Override
     public String toString() {
-        return x + " " + y + " " + direction;
+        return position.toString() + " " + direction.toString();
     }
 }
